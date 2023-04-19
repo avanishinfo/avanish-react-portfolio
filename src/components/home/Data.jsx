4fd7ae72948d2fc4526fc61cbfa248223525c73b
avani <avanishmau@gmail.com>
2023-04-19 17:29:06 +0530

    updated code before PWA

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -57,7 +57,7 @@ const Data = () => {
         </h1>
 
         <h3 className="home__subtitle">Software Developer</h3>
-        <p className="home__description">Hey there, i'm avanish developer based in India, and I am very passionate and dedicated to my work.</p>
+        <p className="home__description">Hey there, i am avanish developer based in India, and I am very passionate and dedicated to my work.</p>
 
         <a href="#contact" className="button button--flex">
             Say Hello
